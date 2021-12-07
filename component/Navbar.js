5a6e6b547fd1ec46e960658e6f38a19ba1722a2b
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2021-12-07 22:33:12 +0000

    Adding cart and signin file

--- component/Navbar.js
+++ component/Navbar.js
@@ -1,39 +1,41 @@
+/* eslint-disable jsx-a11y/role-supports-aria-props */
 import React from 'react'
 import Link from 'next/link'
 
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Features</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown link
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+            <Link href="/">
+            <a className="navbar-brand">CRISHOP</a>
+            </Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                <ul className="navbar-nav">
+                <li className="nav-item active">
+                    <Link href="/cart">
+                    <a className="nav-link"><i className="fas fa-shopping-cart"></i>Cart</a>
+                    </Link>
+                </li>
+                <li className="nav-item active">
+                    <Link href="/signin">
+                    <a className="nav-link"><i className ="fas fa-sign-in-alt"/>Sign</a>
+                    </Link>
+                </li>
+
+                {/* <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    User Name
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a className="dropdown-item" href="/profile">Profile</a>
+                    <a className="dropdown-item" href="/logout">Logout</a>
+                    </div>
+                </li> */}
                 </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+            </div>
+        </nav>
     )
 }
 
