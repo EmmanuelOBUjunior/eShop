238a2084ede47ccc741139ee4b1a2d30ddb57dc0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2021-12-07 22:13:16 +0000

    Modifying _document and navbar file

--- pages/_document.js
+++ pages/_document.js
@@ -7,11 +7,11 @@ class MyDocument extends Document{
             <Html lang = "en">
                 <Head>
                     <meta name="description" content="E-Commerce Shop" />
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" ></link>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" ></script>
-                </Head>
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
+                    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
+                    </Head>
                 <body>
                     <Main/>
                     <NextScript/>
